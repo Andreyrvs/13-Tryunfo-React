fb576c8673b441215d2c5c4653ebd6c4c77e5522
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-11-01 22:57:04 -0300

    chore: config do tailwindO

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+/* eslint-disable max-len */
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      inset: {
+        '-349': '-21.813rem',
+        '-365': '-22.813rem',
+      },
       fontFamily: {
         epi: 'Epilogue',
         verdana: 'Verdana',
@@ -10,7 +15,7 @@ module.exports = {
       colors: {
         primary: '#023031',
         pwhite: '#fff',
-        primarText: '#2FC18C',
+        primarText: 'var(--bg-primary)',
         secondText: '#3D495C',
       },
       spacing: {
@@ -18,12 +23,15 @@ module.exports = {
         404: '25.25rem',
         721: '45.063rem',
         1124: '70.25rem',
+        1132: '70.875rem',
         card: '94.311%',
         cardGap: '6.4%',
       },
       fontSize: {
         '34xl': '1.75rem',
       },
+      backgroundColor: ['checked'],
+      borderColor: ['checked'],
     },
   },
   variants: {
